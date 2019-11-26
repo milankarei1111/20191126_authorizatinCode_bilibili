ec34f274d261e5bbf950be74527be55b23f40e9a
wanru_tsai <milankarei1111@gmail.com>
2019-11-26 11:47:26 +0800

    1.建立bili登入頁面 2.建立第三方登入重定向

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// $ php artisan passport:client 設定的值
+$clientId = 3;
+$clientSecret = 'MFUo1AcSf2mM8dMXUxrDjr11NO37FwtwxyNq3Vr4';
+
+// bili登入頁面
+Route::view('/login', 'auth.login');
+
+// 第三方登入重定向
+Route::get('lishen/login',
+    function (\Illuminate\Http\Request $request) use ($clientId) {
+        $request->session()->put('state', $state = \Illuminate\Support\Str::random(40));
+
+        $query = http_build_query([
+            'client_id' => $clientId,
+            'redirect_uri' => 'http://localhost:9987/auth/callback',
+            'response_type' => 'code',
+            'scope' => '*',
+            'state' => $state,
+        ]);
+
+        return redirect('http://localhost:9988/oauth/authorize?'.$query);
+});
